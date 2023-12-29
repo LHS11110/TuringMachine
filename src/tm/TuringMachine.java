ee2da415098c3d2c2de8f47ba035602008fec207
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-12-29 14:14:06 +0900

    Update TuringMachine.java

--- src/tm/TuringMachine.java
+++ src/tm/TuringMachine.java
@@ -1,4 +1,33 @@
 package tm;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.HashMap;
 
-public interface TuringMachine {
+public class TuringMachine
+{
+    // Direction
+    public final boolean R = true;
+    public final boolean L = false;
+
+    private LinkedList<String> tape;
+    private Set<String> symbols;
+    private Set<String> states;
+    private String blankSymbol;
+    private String initialState;
+    private Set<String> finalStates;
+    private String inputSymbols;
+    private HashMap<String[], String[]> transitionFunction;
+
+    public TuringMachine()
+    {
+        this.tape = new LinkedList<>();
+        this.symbols = new HashSet<>();
+        this.states = new HashSet<>();
+        this.blankSymbol = null;
+        this.initialState = null;
+        this.finalStates = new HashSet<>();
+        this.inputSymbols = null;
+        this.transitionFunction = new HashMap<>();
+    }
 }
